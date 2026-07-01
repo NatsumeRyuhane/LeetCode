ea4320d039a1c7f5a816dcc3fc47322cd587b3e4
Ryuhane <git@ryuhane.me>
2026-07-02 01:30:47 +0800

    wip(2812): per-cell distance in frontier + FIFO pop for safeness BFS

--- sessions/2812-find-the-safest-path-in-a-grid/solution.py
+++ sessions/2812-find-the-safest-path-in-a-grid/solution.py
@@ -2,6 +2,7 @@ from typing import List, TypeAlias
 
 Grid: TypeAlias = List[List[int]]
 
+
 class Solution:
     def maximumSafenessFactor(self, grid: Grid) -> int:
         SFM = self.generateSafenessFactorMatrix(grid)
@@ -14,14 +15,13 @@ class Solution:
         safeness_list = sorted(list(safeness_set), reverse=True)
         max_safeness = max(
             safeness_list[0], SFM[0][0], SFM[len(grid)-1][len(grid)-1])
-        
+
         index = safeness_list.index(max_safeness)
         while not self.TryConstructPath(SFM, safeness_list[index]):
             index += 1
 
         return safeness_list[index]
 
-    
     def TryConstructPath(self, SFM: Grid, max_safeness: int) -> bool:
         def constructPathBFS(cell_in_path: set[tuple[int, int]], pos: tuple[int, int]) -> bool:
             if pos[0] not in range(0, len(SFM[0])) or pos[1] not in range(0, len(SFM)):
@@ -29,27 +29,23 @@ class Solution:
 
             if SFM[pos[0]][pos[1]] < max_safeness:
                 return False
-            
+
             if pos in cell_in_path:
                 return False
-            
+
             if pos[0] == len(SFM[0])-1 and pos[1] == len(SFM[0])-1:
                 return True
-        
+
             cell_in_path.add(pos)
-            return(
+            return (
                 constructPathBFS(cell_in_path, (pos[0]-1, pos[1])) or
                 constructPathBFS(cell_in_path, (pos[0]+1, pos[1])) or
                 constructPathBFS(cell_in_path, (pos[0], pos[1]-1)) or
                 constructPathBFS(cell_in_path, (pos[0], pos[1]+1))
             )
-        
+
         return constructPathBFS(set(), (0, 0))
-            
 
-
-        
-    
     def generateSafenessFactorMatrix(self, grid: Grid) -> Grid:
         SFM: Grid = []
         for i in range(0, len(grid[0])):
@@ -58,35 +54,30 @@ class Solution:
                 SFM[i].append(self.thiefBFS((i, j), grid))
 
         return SFM
-            
 
     def thiefBFS(self, pos: tuple[int, int], grid: Grid) -> int:
-        def BFSProcess(distance: int, grid: Grid, checked: set[tuple[int, int]], queue: List[tuple[int, int]]) -> int:
+        def BFSProcess(grid: Grid, checked: set[tuple[int, int]], queue: List[tuple[int, int, int]]) -> int:
             while not len(queue) == 0:
-                pos = queue.pop()
+                pos = queue.pop(0)
 
                 if pos[0] not in range(0, len(grid[0])) or pos[1] not in range(0, len(grid[0])):
                     continue
-                
+
                 if pos in checked:
                     continue
-                
+
                 if grid[pos[0]][pos[1]] == 1:
-                    return distance
-                
+                    return pos[2]
+
                 else:
-                    checked.add(pos)
-                    distance += 1
-            
-                    queue.append((pos[0]+1, pos[1]))
-                    queue.append((pos[0]-1, pos[1]))
-                    queue.append((pos[0], pos[1]+1))
-                    queue.append((pos[0], pos[1]-1))
+                    checked.add((pos[0], pos[1]))
+                    distance = pos[2]
+                    queue.append((pos[0]+1, pos[1], distance+1))
+                    queue.append((pos[0]-1, pos[1], distance+1))
+                    queue.append((pos[0], pos[1]+1, distance+1))
+                    queue.append((pos[0], pos[1]-1, distance+1))
 
             # should not be possible
             return -1
-                
-            
-        return BFSProcess(0, grid, set(), [pos])
-            
 
+        return BFSProcess(grid, set(), [(pos[0], pos[1], 0)])
